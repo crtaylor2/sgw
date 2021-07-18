05a3eb6a2284bd5eecfa2ce670490456c61d4858
Cariessa Taylor <cariessataylor@gmail.com>
2021-07-17 22:10:51 -0600

    Added comments

--- SpreadGodsWord.py
+++ SpreadGodsWord.py
@@ -1,7 +1,31 @@
+######################################################################
+#
+# Cariessa Taylor
+# July 17, 2021
+# CSIS 483 Phase 1
+#
+# This project serves as a platform to retreive the scriptures in
+# various languages and translations. This program can run in two
+# different methods:
+#  1. Stand Alone - useful for development, testing, and single
+#     user situations
+#  2. REST HTTP server - useful as a backend server to provide
+#     HTTP based REST APIs
+#
+# For more details, see the README.md file.
+#
+######################################################################
+
 import argparse
 import json
 import flask
 
+app = flask.Flask(__name__)
+
+######################################################################
+# This class performs the actions for the language requests (i.e. what
+# languages are supported by this program?)
+######################################################################
 class LanguageAction(argparse.Action):
     def languagesAsJson():
         #get file object
@@ -23,6 +47,10 @@ class LanguageAction(argparse.Action):
         print(LanguageAction.languagesAsJson())
 
 
+######################################################################
+# This class performs the actions for the translations request (i.e.
+# what translations are supported by this program?)
+######################################################################
 class TranslationAction(argparse.Action):
     def translationsAsJson():
         #get file object
@@ -58,8 +86,9 @@ class TranslationAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
        print(TranslationAction.translationsAsJson())
 
-app = flask.Flask(__name__)
-
+######################################################################
+# This class performs the HTTP server actions for the program
+######################################################################
 class ServerAction(argparse.Action):
     @app.route('/languages')
     def languages():
@@ -73,15 +102,23 @@ class ServerAction(argparse.Action):
        app.run(debug=True)
 
 
-def parseArguments():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-l', '--languages', action=LanguageAction, nargs=0, help='show languages supported and exit')
-    parser.add_argument('-t', '--translations', action=TranslationAction, nargs=0, help='show translations supported and exit')
-    parser.add_argument('-s', '--server', action=ServerAction, nargs=0, help='run as a REST server')
-    args = parser.parse_args()
+######################################################################
+# This class functions as the main class. It parses the arguments and
+# calls the supporting functions to accomplish its goal.
+######################################################################
+class SpreadGodsWord:
+    def parseArguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-l', '--languages', action=LanguageAction, nargs=0,
+            help='show languages supported and exit')
+        parser.add_argument('-t', '--translations', action=TranslationAction, nargs=0,
+            help='show translations supported and exit')
+        parser.add_argument('-s', '--server', action=ServerAction, nargs=0,
+            help='run as a REST server')
+        args = parser.parse_args()
 
-def main():
-    parseArguments()
+    def main():
+        SpreadGodsWord.parseArguments()
 
 if __name__ == "__main__":
-    main()
+    SpreadGodsWord.main()
